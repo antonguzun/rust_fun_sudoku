524651393ab8a9f5f3f36a1d6207c4ba1c6965ec
antonguzun <anton.guzun@fincollect.cz>
2020-10-03 21:53:47 +0300

    finish rules checker

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::char;
 extern crate ncurses;
 use ncurses::*;
 
@@ -8,9 +7,7 @@ struct SudokuTable {
 
 impl SudokuTable {
     fn new() -> SudokuTable {
-        SudokuTable {
-            array: [[0u8; 9]; 9],
-        }
+        SudokuTable { array: [[0; 9]; 9] }
     }
     fn display_view(&self, cursor: &Position) -> String {
         let mut string_data = String::new();
@@ -34,10 +31,48 @@ impl SudokuTable {
         }
         string_data
     }
+    fn find_first_chunk_index(&self, current_index: &usize) -> usize {
+        match current_index {
+            0..=2 => 0,
+            3..=5 => 3,
+            6..=8 => 6,
+            _ => panic!("Wrong input index"),
+        }
+    }
+    fn check_rules(&self, value: u8, position: &Position) -> bool {
+        if value == 0 {
+            return true;
+        }
+        for row in self.array.iter() {
+            if row[position.y] == value {
+                return false;
+            }
+        }
+        for column_value in self.array[position.x].iter() {
+            if column_value == &value {
+                return false;
+            }
+        }
+        let x_chunk_start_index = self.find_first_chunk_index(&position.x);
+        let y_chunk_start_index = self.find_first_chunk_index(&position.y);
+        for row in self.array[x_chunk_start_index..=x_chunk_start_index + 2].iter() {
+            for column_value in row[y_chunk_start_index..=y_chunk_start_index + 2].iter() {
+                if column_value == &value {
+                    return false;
+                }
+            }
+        }
+        true
+    }
     fn set_value(&mut self, value: String, position: &Position) {
         let parsing_result = value.trim().parse();
         match parsing_result {
-            Ok(0..=9) => self.array[position.x][position.y] = parsing_result.unwrap(),
+            Ok(0..=9) => {
+                let number = parsing_result.unwrap();
+                if self.check_rules(number, &position) {
+                    self.array[position.x][position.y] = number;
+                }
+            }
             _ => {}
         }
     }
@@ -81,10 +116,11 @@ fn main() {
     initscr();
     let mut table: SudokuTable = SudokuTable::new();
     let mut position: Position = Position::new();
+    let help_string = "move cursor: [w, a, s, d]\nset value: enter, press digit, enter\n";
     loop {
-        addstr(&table.display_view(&position));
-        refresh();
+        addstr(help_string);
         addstr(&position.display_position());
+        addstr(&table.display_view(&position));
         let ch = getch();
         match ch {
             113 => break,
